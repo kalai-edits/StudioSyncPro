bc67d01113056d01f17098fbfa678b23e3d3a99f
kalai-edits <kalaiedits007@email.com>
2026-03-31 22:34:20 +0530

    messsage overlay size adjust

--- client/src/layout/MsgLayout.jsx
+++ client/src/layout/MsgLayout.jsx
@@ -10,7 +10,7 @@ export default function MsgLayout() {
       <TbMessageCircleHeart
         className="bg-transparent border-none outline-none text-[#19e083]  hover:scale-110   transition-all  duration-300  right-8 bottom-10
 absolute md:right-10 drop-shadow-[0_0_12px_rgba(94,234,212,0.5)]  hover:drop-shadow-[0_0_25px_rgba(94,234,212,0.9)]"
-        size={110}
+        size={90}
         onClick={() => setIsOpen(true)}
       />
       <Message isOpen={isOpen} onClose={() => setIsOpen(false)} />
